cf5706c91a01e780a486f767d53851432b478c50
littwa <littwa777@gmail.com>
2020-05-14 22:54:46 +0300

    hw-11 v2

--- hw-11/goit-js-hw-11-timer/index.js
+++ hw-11/goit-js-hw-11-timer/index.js
@@ -6,24 +6,36 @@ class CountdownTimer {
   this.hoursDisolay = document.querySelector('[data-value="hours"]');
   this.minsDisolay = document.querySelector('[data-value="mins"]');
   this.secsDisolay = document.querySelector('[data-value="secs"]');
-  document
-   .querySelector(this.selector)
-   .insertAdjacentText("afterbegin", `Until  ${targetDate.toDateString()} :`);
+  this.timeString = targetDate.toDateString();
+  this.container = document.querySelector(this.selector);
+  this.container.insertAdjacentText("afterbegin", `Until  ${this.timeString} :`);
+ }
+
+ timerCount() {
+  this.curentDate = new Date().getTime();
+  this.time = this.targetDate - this.curentDate;
+  this.days = Math.floor(this.time / (1000 * 60 * 60 * 24));
+  this.hours = Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  this.mins = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
+  this.secs = Math.floor((this.time % (1000 * 60)) / 1000);
+  this.daysDisolay.textContent = this.days;
+  this.hoursDisolay.textContent = this.hours;
+  this.minsDisolay.textContent = this.mins;
+  this.secsDisolay.textContent = this.secs;
  }
 
  startTimer() {
-  setInterval(() => {
-   this.curentDate = new Date().getTime();
-   this.time = this.targetDate - this.curentDate;
-   this.days = Math.floor(this.time / (1000 * 60 * 60 * 24));
-   this.hours = Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   this.mins = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
-   this.secs = Math.floor((this.time % (1000 * 60)) / 1000);
-   this.daysDisolay.textContent = this.days;
-   this.hoursDisolay.textContent = this.hours;
-   this.minsDisolay.textContent = this.mins;
-   this.secsDisolay.textContent = this.secs;
-  }, 1000);
+  this.timerCount();
+
+  if (this.time <= 0) {
+   document.querySelectorAll("span").forEach(el => (el.textContent = ""));
+   this.container.textContent = `It's time! ${this.timeString}`;
+   return;
+  } else {
+   setInterval(() => {
+    this.timerCount();
+   }, 1000);
+  }
  }
 }
 
